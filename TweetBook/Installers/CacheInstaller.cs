c34f4da2cad54d86f15a312eea78dfd3d951fbb9
Maria Crucin <maria.crucin@fortech.ro>
2022-03-03 15:00:41 +0200

    Response caching using Redis

---
+++ TweetBook/Installers/CacheInstaller.cs
@@ -0,0 +1,23 @@
+﻿using TweetBook.Cache;
+using TweetBook.Services;
+
+namespace TweetBook.Installers
+{
+    public class CacheInstaller : IInstaller
+    {
+        public void InstallServices(WebApplicationBuilder builder)
+        {
+            var redisCacheSettings = new RedisCacheSettings();
+            builder.Configuration.GetSection(nameof(RedisCacheSettings)).Bind(redisCacheSettings);
+            builder.Services.AddSingleton(redisCacheSettings);
+
+            if (!redisCacheSettings.Enabled)
+            {
+                return;
+            }
+
+            builder.Services.AddStackExchangeRedisCache(options => options.Configuration = redisCacheSettings.ConnectionString);
+            builder.Services.AddSingleton<IResponseCacheService, ResponseCacheService>();
+        }
+    }
+}
